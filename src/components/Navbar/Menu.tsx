91ef5f285fce4184fe97696d6a1aced4f72803af
Patri22k <bajzik.patrik6@gmail.com>
2024-08-27 21:43:32 +0200

    Stuff for navbar, one error with z-index is occuring. styled "Contact Me" button

--- src/components/Navbar/Menu.tsx
+++ src/components/Navbar/Menu.tsx
@@ -1,15 +1,12 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 import { DownOutlined } from '@ant-design/icons'
-import { useState } from "react";
-
-// Styling almost complete
-// TODO: Add to 480px and above 2 cols for pop-up window on hover
 
 const Menu = () => {
     const [style, setStyle] = useState({ display: 'none' });
 
     return (
-        <div className="menu-container z-40">
+        <div className="menu-container">
             <div className="link-container pb-2 w-auto h-auto">
                 <div className="link-wrapper flex gap-x-1 pt-5 pb-3">
                     <DownOutlined className="py-auto" />
@@ -29,21 +26,21 @@ const Menu = () => {
                     <DownOutlined className="py-auto" />
                 </div>
 
-                {/* Pop-up window on hover */}
+                {/* Pop-up window on hover for tablets and below */}
                 <div className="menu-projects-container" style={style}>
-                    <div className="menu-projects-wrapper flex flex-col">
+                    <div className="menu-projects-wrapper flex flex-col ml-[3%] min-[480px]:grid min-[480px]:grid-cols-2 min-[480px]:grid-rows-1 min-[480px]:gap-x-16 sm:gap-x-20 md:gap-x-56">
                         <div className="subproject-container flex gap-x-2 border-b-2 border-b-black py-4">
                             <h3 className="text-black/50">/01</h3>
                             <h1 className="text-2xl font-medium">GitHub Projects</h1>
                             <div className="arrow-container flex justify-center items-center w-8 h-8">
-                                <img className="w-8 h-8" src="https://cdn.prod.website-files.com/66bf407255e2bbac50fde356/66bf9e7b27401f68266d92b9_Arrow%20-%20Right.svg" alt="Arrow"></img>
+                                <img src="https://cdn.prod.website-files.com/66bf407255e2bbac50fde356/66bf9e7b27401f68266d92b9_Arrow%20-%20Right.svg" alt="Arrow"></img>
                             </div>
                         </div>
                         <div className="subproject-container flex gap-x-2 border-b-2 border-b-black py-4">
                             <h3 className="text-black/50">/02</h3>
                             <h1 className="text-2xl font-medium">School Projects</h1>
                             <div className="arrow-container flex justify-center items-center w-8 h-8">
-                                <img className="w-8 h-8" src="https://cdn.prod.website-files.com/66bf407255e2bbac50fde356/66bf9e7b27401f68266d92b9_Arrow%20-%20Right.svg" alt="Arrow"></img>
+                                <img src="https://cdn.prod.website-files.com/66bf407255e2bbac50fde356/66bf9e7b27401f68266d92b9_Arrow%20-%20Right.svg" alt="Arrow"></img>
                             </div>
                         </div>
                     </div>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,8 +5,12 @@ import { DownOutlined } from '@ant-design/icons'
 import { useState } from "react"
 import Menu from "./Menu.tsx"
 
+// Z-index not working for Laptops Menu Display
+// The hero section overrides the pop-up menu
+
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [style, setStyle] = useState({ display: 'none' });
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
@@ -14,7 +18,7 @@ const Navbar = () => {
 
     return (
         <div className="navigation-container">
-            <nav className="navigation-inner-container block relative">
+            <nav className="navigation-inner-container block  relative">
                 <div className="navigation-wrapper flex justify-between items-center py-3 mx-auto w-[90%] max-w-[1340px] lg:w-[95%]">
 
                     {/* Logo Section */}
@@ -25,18 +29,31 @@ const Navbar = () => {
                     {/* NavLinks for Desktop */}
                     <div className="navbar-links-container hidden lg:block">
                         <div className="navigation-links-wrapper flex gap-x-8 w-auto h-auto">
+
+                            {/* Home */}
                             <div className="link-container pb-2 w-auto h-auto">
                                 <div className="link-wrapper flex justify-center gap-x-1 pt-5 pb-3">
                                     <DownOutlined className="py-auto" />
                                     <Link to="/" className="font-semibold text-xl">Home</Link>
                                 </div>
                             </div>
+
+                            {/* Projects */}
                             <div className="link-container pb-2 w-auto h-auto">
-                                <div className="link-wrapper flex justify-center gap-x-1 pt-5 pb-3">
+                                <div
+                                    className="link-wrapper flex justify-center gap-x-1 pt-5 pb-3"
+                                    onMouseEnter={() => {
+                                        setStyle({ display: "block" });
+                                    }}
+                                    onMouseLeave={() => {
+                                        setStyle({ display: "none" });
+                                    }}>
                                     <Link to="/projects" className="font-semibold text-xl">Projects</Link>
                                     <DownOutlined className="py-auto" />
                                 </div>
                             </div>
+
+                            {/* Contact */}
                             <div className="link-container pb-2 w-auto h-auto">
                                 <div className="link-wrapper flex justify-center gap-x-1 pt-5 pb-3">
                                     <Link to="/contact" className="font-semibold text-xl">Contact</Link>
@@ -60,6 +77,28 @@ const Navbar = () => {
                     <Menu />
                 </div>
             )}
+
+            {/* Laptops Menu Display */}
+            <div className="menu-projects-container relative" style={style}>
+                <div className="menu-inner-container absolute h-full w-full">
+                    <div className="menu-projects-wrapper flex justify-evenly shadow-2xl pb-20">
+                        <div className="subproject-container flex gap-x-2 border-b-2 border-b-black py-4">
+                            <h3 className="text-black/50">/01</h3>
+                            <h1 className="text-2xl font-medium">GitHub Projects</h1>
+                            <div className="arrow-container flex justify-center items-center w-8 h-8">
+                                <img src="https://cdn.prod.website-files.com/66bf407255e2bbac50fde356/66bf9e7b27401f68266d92b9_Arrow%20-%20Right.svg" alt="Arrow"></img>
+                            </div>
+                        </div>
+                        <div className="subproject-container flex gap-x-2 border-b-2 border-b-black py-4">
+                            <h3 className="text-black/50">/02</h3>
+                            <h1 className="text-2xl font-medium">School Projects</h1>
+                            <div className="arrow-container flex justify-center items-center w-8 h-8">
+                                <img src="https://cdn.prod.website-files.com/66bf407255e2bbac50fde356/66bf9e7b27401f68266d92b9_Arrow%20-%20Right.svg" alt="Arrow"></img>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
