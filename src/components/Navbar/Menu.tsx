6c605d2e2fb2ca8facac0daaf01beadfb29e7315
Patri22k <bajzik.patrik6@gmail.com>
2024-08-31 23:49:55 +0200

    Changing navbar to the simplest design with no errors, added backup file for old-navbar, but is not included for commiting

--- src/components/Navbar/Menu.tsx
+++ src/components/Navbar/Menu.tsx
@@ -1,59 +1,25 @@
-import { useState } from "react"
 import { Link } from "react-router-dom"
 import { DownOutlined } from '@ant-design/icons'
 
 const Menu = () => {
-    const [style, setStyle] = useState({ display: 'none' });
-
     return (
         <div className="menu-container">
             <div className="link-container pb-2 w-auto h-auto">
                 <div className="link-wrapper flex gap-x-1 pt-5 pb-3">
-                    <DownOutlined className="py-auto" />
                     <Link to="/" className="font-semibold text-base xs:text-xl">Home</Link>
+                    <DownOutlined className="py-auto" />
                 </div>
             </div>
-            <div
-                className="link-container pb-2 w-auto h-auto"
-                onMouseEnter={() => {
-                    setStyle({ display: "block" });
-                }}
-                onMouseLeave={() => {
-                    setStyle({ display: "none" })
-                }}>
+            <div className="link-container pb-2 w-auto h-auto">
                 <div className="link-wrapper flex gap-x-1 pt-5 pb-3">
                     <Link to="/projects" className="font-semibold text-base xs:text-xl">Projects</Link>
                     <DownOutlined className="py-auto" />
                 </div>
-
-                {/* Pop-up window on hover for tablets and below */}
-                <div className="menu-projects-container" style={style}>
-                    <div className="menu-projects-wrapper flex flex-col ml-[3%] min-[480px]:grid min-[480px]:grid-cols-2 min-[480px]:grid-rows-1 min-[480px]:gap-x-16 sm:gap-x-20 md:gap-x-56">
-                        <div className="subproject-container flex gap-x-2 border-b-2 border-b-black py-4">
-                            <Link to="/projects">
-                                <h3 className="text-black/50">/01</h3>
-                                <h1 className="text-2xl font-medium">GitHub Projects</h1>
-                                <div className="arrow-container flex justify-center items-center w-8 h-8">
-                                    <img src="https://cdn.prod.website-files.com/66bf407255e2bbac50fde356/66bf9e7b27401f68266d92b9_Arrow%20-%20Right.svg" alt="Arrow"></img>
-                                </div>
-                            </Link>
-                        </div>
-                        <div className="subproject-container flex gap-x-2 border-b-2 border-b-black py-4">
-                            <Link to="/projects">
-                                <h3 className="text-black/50">/02</h3>
-                                <h1 className="text-2xl font-medium">School Projects</h1>
-                                <div className="arrow-container flex justify-center items-center w-8 h-8">
-                                    <img src="https://cdn.prod.website-files.com/66bf407255e2bbac50fde356/66bf9e7b27401f68266d92b9_Arrow%20-%20Right.svg" alt="Arrow"></img>
-                                </div>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-
             </div>
             <div className="link-container pb-2 w-auto h-auto">
                 <div className="link-wrapper flex gap-x-1 pt-5 pb-3">
                     <Link to="/contact" className="font-semibold text-base xs:text-xl">Contact</Link>
+                    <DownOutlined className="py-auto" />
                 </div>
             </div>
         </div>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,11 @@
-import React, { useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 import { DownOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import profilePicture from "../../assets/Profile-picture-without-me.jpg";
 import MenuIcon from "./MenuIcon.tsx";
 import Menu from "./Menu.tsx";
 
-/**
- * Error: Positioning of the dropdown menu for laptops and above, where it overflows
- */
-
-type NavbarProps = React.PropsWithChildren & { FlyoutContent?: React.FC };
-
-const Example = () => {
+const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleMenu = () => {
@@ -35,23 +28,22 @@ const Example = () => {
 
                             {/* Home */}
                             <div className="link-wrapper flex justify-center gap-x-1 pt-5 pb-3">
-                                <DownOutlined className="py-auto" />
                                 <Link to="/" className="font-semibold text-xl">Home</Link>
+                                <DownOutlined className="py-auto" />
                             </div>
 
                             {/* Projects */}
                             <div className="link-wrapper pt-5 pb-3">
-                                <FlyoutLink FlyoutContent={ProjectTypes}>
-                                    <div className="flex justify-center gap-x-1">
-                                        <Link to="/projects" className="font-semibold text-xl">Projects</Link>
-                                        <DownOutlined className="py-auto" />
-                                    </div>
-                                </FlyoutLink>
+                                <div className="flex justify-center gap-x-1">
+                                    <Link to="/projects" className="font-semibold text-xl">Projects</Link>
+                                    <DownOutlined className="py-auto" />
+                                </div>
                             </div>
 
                             {/* Contact */}
                             <div className="link-wrapper flex justify-center gap-x-1 pt-5 pb-3">
                                 <Link to="/contact" className="font-semibold text-xl">Contact</Link>
+                                <DownOutlined className="py-auto" />
                             </div>
                         </div>
                     </div>
@@ -77,69 +69,4 @@ const Example = () => {
     );
 };
 
-const FlyoutLink: React.FC<NavbarProps> = ({ children, FlyoutContent }) => {
-    const [open, setOpen] = useState(false);
-
-    const showFlyout = FlyoutContent && open;
-
-    return (
-        <div
-            onMouseEnter={() => setOpen(true)}
-            onMouseLeave={() => setOpen(false)}
-            className="relative w-fit h-fit"
-        >
-            <div className="relative">
-                {children}
-                <span
-                    style={{
-                        transform: showFlyout ? "scaleX(1)" : "scaleX(0)",
-                    }}
-                    className="absolute -bottom-2 -left-2 -right-2 h-1 origin-left scale-x-0 rounded-full bg-orange-400 transition-transform duration-300 ease-out"
-                />
-            </div>
-            <AnimatePresence>
-                {showFlyout && (
-                    <motion.div
-                        initial={{ opacity: 0, y: 15 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        exit={{ opacity: 0, y: 15 }}
-                        transition={{ duration: 0.3, ease: "easeOut" }}
-                        className="absolute top-12 w-[calc(100vw-20px)] !-translate-x-[78vw] bg-white z-10 2xl:w-[calc(100vw-8px)] 2xl:!-translate-x-[73vw]"
-                    >
-                        <div className="absolute -top-6 left-0 right-0 h-6 bg-transparent" />
-                        <FlyoutContent />
-                    </motion.div>
-                )}
-            </AnimatePresence>
-        </div>
-    );
-};
-
-const ProjectTypes = () => {
-    return (
-        <div className="menu-projects-container">
-            <div className="menu-projects-wrapper flex justify-evenly shadow-2xl pb-8">
-                <Link to="/projects">
-                    <div className="subproject-container flex gap-x-2 border-b-2 border-b-black py-4">
-                        <h3 className="text-black/50">/01</h3>
-                        <h1 className="text-2xl font-medium">GitHub Projects</h1>
-                        <div className="arrow-container flex justify-center items-center w-8 h-8">
-                            <img src="https://cdn.prod.website-files.com/66bf407255e2bbac50fde356/66bf9e7b27401f68266d92b9_Arrow%20-%20Right.svg" alt="Arrow"></img>
-                        </div>
-                    </div>
-                </Link>
-                <Link to="/projects">
-                    <div className="subproject-container flex gap-x-2 border-b-2 border-b-black py-4">
-                        <h3 className="text-black/50">/02</h3>
-                        <h1 className="text-2xl font-medium">School Projects</h1>
-                        <div className="arrow-container flex justify-center items-center w-8 h-8">
-                            <img src="https://cdn.prod.website-files.com/66bf407255e2bbac50fde356/66bf9e7b27401f68266d92b9_Arrow%20-%20Right.svg" alt="Arrow"></img>
-                        </div>
-                    </div>
-                </Link>
-            </div>
-        </div>
-    );
-};
-
-export default Example;
+export default Navbar;
